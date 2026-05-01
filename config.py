40f7a5a978dd018130332c0b7c46dce0966f0072
swapan9641 <xyz686@atomicmail.io>
2026-05-01 08:34:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 MONGO_URI = os.environ.get("MONGO_URI", "")
 DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "0"))
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]
-CREDIT_TEXT = os.environ.get("DEVIL6", "\n\n**Downloaded by @mega_nzleech_bot")
+CREDIT_TEXT = os.environ.get("BOT_ZONE", "\n\n**Downloaded by @mega_nzleech_bot")
 DOWNLOAD_DIR = "downloads"
+PORT = int(os.environ.get("PORT", "8080")) # Added Port Variable
 
 if not os.path.exists(DOWNLOAD_DIR):
     os.makedirs(DOWNLOAD_DIR)
