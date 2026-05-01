a9d3e85ed8f17f6db25d6e9feb78ab3902247f50
swapan9641 <xyz686@atomicmail.io>
2026-05-01 08:09:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+MONGO_URI = os.environ.get("MONGO_URI", "")
+DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "0"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]
+CREDIT_TEXT = os.environ.get("CREDIT_TEXT", "\n\n**Downloaded by @YourBot**")
+DOWNLOAD_DIR = "downloads"
+
+if not os.path.exists(DOWNLOAD_DIR):
+    os.makedirs(DOWNLOAD_DIR)
